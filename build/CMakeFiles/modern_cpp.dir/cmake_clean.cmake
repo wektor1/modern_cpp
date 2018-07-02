f72351373ecd38707ee3957df981ee479400d3e5
wektor1 <wektor1@gmail.com>
2018-07-03 01:41:59 +0200

    Another Makefile

---
+++ build/CMakeFiles/modern_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/modern_cpp.dir/Square.cpp.o"
+  "CMakeFiles/modern_cpp.dir/Rectangle.cpp.o"
+  "CMakeFiles/modern_cpp.dir/main.cpp.o"
+  "CMakeFiles/modern_cpp.dir/Circle.cpp.o"
+  "CMakeFiles/modern_cpp.dir/Shape.cpp.o"
+  "modern_cpp.pdb"
+  "modern_cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/modern_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
